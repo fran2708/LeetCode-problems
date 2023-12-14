47b4f91a5abc19540728273adafa848d1e743c63
fran2708 <francopaladini2708@gmail.com>
2023-12-13 22:54:28 -0300

    added second attempt

--- 14 - Longest Common Prefix/solution.js
+++ 14 - Longest Common Prefix/solution.js
@@ -30,3 +30,35 @@ var longestCommonPrefix = function(strs) {
 
 // this is not performant at all, will try to improve
 // but it works...
+
+// the following version is more performant (57 ms vs 61 ms) but still not ideal
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+    if (strs.length === 0) {
+        return ""
+    }
+    let stringArrays = []
+    let result = ""
+    for (let i = 0; i < strs.length; i++) {
+        stringArrays.push(strs[i].split(''))
+        if (stringArrays[i][0] === undefined) {
+            return ""
+        }
+    }
+
+    let comparison = stringArrays[0]
+
+    for (let i = 0; i < comparison.length; i++) {
+        for (let y = 1; y < strs.length; y++) {
+            if (comparison[i] !== stringArrays[y][i]) {
+                return result
+            }
+        }
+        result += comparison[i]
+    }
+    return result
+};
