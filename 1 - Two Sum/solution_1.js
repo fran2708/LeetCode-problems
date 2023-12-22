0250d6c77f71d5e7d9e0549d2c623f756f090631
fran2708 <francopaladini2708@gmail.com>
2023-12-22 00:20:49 -0300

    first solution for problem 1

---
+++ 1 - Two Sum/solution_1.js
@@ -0,0 +1,20 @@
+// first solution
+// its O(n^2) time complexity, so...
+// it works i guess...
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 1; j < nums.length; j++) {
+            if (i !== j) {
+                if (nums[i] + nums[j] === target) {
+                    return [i,j]
+                }
+            }
+        }   
+    }
+};
